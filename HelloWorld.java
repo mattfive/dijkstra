cde9df08c8005a29443d2dd30eb61c08d3366dcd
mattfive <go.matt@web.de>
2015-12-09 10:15:00 +0100

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class HelloWorld {
-   
-   public static void main(String[] args) {
-      JOptionPane.showMessageDialog( null, "Hello World!" );
-   }
-
-}
